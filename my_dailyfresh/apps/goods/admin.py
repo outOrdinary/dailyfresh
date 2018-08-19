b80d3a40a46ac5d028a697ad8dd3f336c7df7b9d
outOrdinary <373000913@qq.com>
2018-08-19 11:23:04 +0800

    complete

--- my_dailyfresh/apps/goods/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from apps.goods.models import Goods,GoodsType
-
-# Register your models here.
-
-admin.site.register(Goods)
-admin.site.register(GoodsType)
--- my_dailyfresh/apps/goods/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from apps.goods.views import IndexView
-
-urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-]
